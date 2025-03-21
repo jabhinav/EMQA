0a922977c1f4fc11617c29e5830325b0a5755d68
Abhinav Jain <jabhinaviitk@gmail.com>
2025-03-21 18:33:41 -0500

    in-progress support video llava

--- utils/custom.py
+++ utils/custom.py
@@ -11,6 +11,9 @@ import torch
 from pynvml import *
 from transformers import set_seed as _set_seed
 
+from dataclasses import dataclass, field
+from typing import Literal, Tuple
+
 logger = logging.getLogger(__name__)
 
 
@@ -155,3 +158,30 @@ def set_seed(args):
 	"""set random seed."""
 	_set_seed(args.seed)
 
+
+@dataclass
+class Instance:
+	# Src: https://github.com/EvolvingLMMs-Lab/lmms-eval/blob/main/lmms_eval/api/instance.py
+	request_type: Literal["loglikelihood", "generate_until", "generate_until_multi_round"]
+	arguments: tuple
+	idx: int
+	metadata: Tuple[str, int, int] = field(default_factory=lambda: (None, None, None))  # TODO: better typehints here
+	resps: list = field(default_factory=list)
+	filtered_resps: dict = field(default_factory=dict)
+
+	# initialized after init
+	task_name: str = None
+	doc_id: str = None
+	repeats: str = None
+	doc: dict = None
+
+	def __post_init__(self) -> None:
+		# unpack metadata field
+		self.task_name, self.doc_id, self.repeats = self.metadata["task"], self.metadata["doc_id"], self.metadata["repeats"]
+
+	@property
+	def args(self):
+		"""
+		Returns (string,) where `string` is the string to calculate loglikelihood over
+		"""
+		return self.arguments if isinstance(self.arguments, tuple) else (self.arguments,)
